76cf233020b628af1c169441e840c68dfe4fcdc6
Ikram UL Mustafa <rao.ikram42@gmail.com>
2021-10-18 03:20:35 +0500

    Update ProductExport.php

--- app/Exports/ProductExport.php
+++ app/Exports/ProductExport.php
@@ -6,13 +6,18 @@ use App\Models\Product;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Maatwebsite\Excel\Concerns\Exportable;
 use Maatwebsite\Excel\Concerns\FromQuery;
+use Maatwebsite\Excel\Concerns\WithHeadings;
 
-class ProductExport implements FromQuery, ShouldQueue
+class ProductExport implements FromQuery, ShouldQueue ,WithHeadings
 {
     use Exportable;
 
+    public function headings(): array
+    {
+        return ['Id','Title','Description','Price'];
+    }
     public function query()
     {
-        return Product::query();
+        return Product::query()->select('id','title','description','price');
     }
 }
